db0e2f11f524ef2bc9db2aeb2fcce29931f1b44e
rayala11 <144870593+rayala11@users.noreply.github.com>
2023-10-19 16:01:36 +0000

    hello

---
+++ arrays/p1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    /* int test_scores[5] {10,20, 30, 40, 50};
+ 
+    cout << "Score index at 0 " << test_scores[0] << endl;
+    cout << "Score index at 1 " << test_scores[1] << endl;
+    cout << "Score index at 2 " << test_scores[2] << endl;
+    cout << "Score index at 3 " << test_scores[3] << endl;
+    cout << "Score index at 4 " << test_scores[4] << endl; */
+    
+    char vowels[] {'a','e','i','o','u'};
+    
+    cout << "The first vowel is " << vowels[0] << endl;
+    cout << "The last vowel is " << vowels[4];
+}
